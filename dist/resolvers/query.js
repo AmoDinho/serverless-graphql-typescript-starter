4bc089e422a1812d22a66c7f009b9d2fd0ccecea
AmoDinho <u14284783@tuks.co.za>
2019-12-18 14:41:00 +0200

    changed the tsconfig

--- dist/resolvers/query.js
+++ dist/resolvers/query.js
@@ -1,6 +1,4 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.hello = (args, context) => {
+export var hello = function (args, context) {
     return "Your GraphQL API is now LIVE!🎈 ";
 };
 //# sourceMappingURL=query.js.map
